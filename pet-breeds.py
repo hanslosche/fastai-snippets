5bc8745bdaf49307e8d8d81e65ca58c33d8f5b69
Hans Lösche <58704289+hanslosche@users.noreply.github.com>
2021-06-16 23:56:43 -0400

    regex test

--- pet-breeds.py
+++ pet-breeds.py
@@ -1,5 +1,9 @@
 from fastai.vision.all import *
 path = untar_data(URLs.PETS)
 
-print(path.ls())
-print((path/'images').ls()[:5])
+#print(path.ls())
+#print((path/'images').ls()[:5])
+
+fname = (path/'images').ls()[0]
+test = re.findall(r'(.+)_\d+.jpg$', fname.name)
+print(test)
